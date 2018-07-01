de68b5bafe05a6ad8e6fc05cbbbadd497d054612
Tim Krull <tkrull.idlewild@gmail.com>
2018-07-01 11:03:57 -0500

    Added files via upload
    Initial commit

---
+++ Destini/Destini/ViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Tim Krull on 06/28/2018.
+//  Copyright (c) 2018 Tim Krull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Our strings
+    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
+    let answer1a = "I\'ll hop in. Thanks for the help!"
+    let answer1b = "Better ask him if he\'s a murderer first."
+    
+    
+    let story2 = "He nods slowly, unphased by the question."
+    let answer2a = "At least he\'s honest. I\'ll climb in."
+    let answer2b = "Wait, I know how to change a tire."
+    
+    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
+    let answer3a = "I love Elton John! Hand him the cassette tape."
+    let answer3b = "It\'s him or me! You take the knife and stab him."
+    
+    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
+    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
+    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
+    
+    
+    // UI Elements linked to the storyboard
+    @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
+    @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
+    @IBOutlet weak var storyTextView: UILabel!
+	@IBOutlet weak var restartButton: UIButton!
+	
+    //Global variable
+	var storyIndex: Int = 1 //index to track story progress
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+        //Call to update the UI
+		updateUI()
+    }
+
+    
+    // User presses one of the buttons
+    @IBAction func buttonPressed(_ sender: UIButton) {
+
+		//Check which button was pressed and update storyIndex
+		if sender.tag == 1 {
+			switch storyIndex {
+			case 1, 2:
+				storyIndex = 3
+			case 3:
+				storyIndex = 6
+			default:
+				print("Error")
+			}
+		}
+		else if sender.tag == 2 {
+			switch storyIndex {
+			case 1:
+				storyIndex = 2
+			case 2:
+				storyIndex = 4
+			case 3:
+				storyIndex = 5
+			default:
+				print("Error")
+			}
+		}
+		else if sender.tag == 3 {
+			restartStory()
+		}
+		else {
+			print("Error")
+		}
+		
+		//Call to update the UI
+		updateUI()
+		
+    }//buttonPressed(_ sender: UIButton)
+	
+	//updates the UI to the current story part and options
+	func updateUI(){
+		
+		switch storyIndex {
+		case 1:
+			//Set storyTextView to the story's starting point
+			storyTextView.text = story1
+			//Set topButton's title to the 1st starting option
+			topButton.setTitle(answer1a, for: UIControlState())
+			//Set bottomButton to the 2nd starting option
+			bottomButton.setTitle(answer1b, for: UIControlState())
+		case 2:
+			storyTextView.text = story2
+			topButton.setTitle(answer2a, for: UIControlState())
+			bottomButton.setTitle(answer2b, for: UIControlState())
+		case 3:
+			storyTextView.text = story3
+			topButton.setTitle(answer3a, for: UIControlState())
+			bottomButton.setTitle(answer3b, for: UIControlState())
+		case 4:
+			storyTextView.text = story4
+			topButton.isHidden = true
+			bottomButton.isHidden = true
+			restartButton.isHidden = false
+		case 5:
+			storyTextView.text = story5
+			topButton.isHidden = true
+			bottomButton.isHidden = true
+			restartButton.isHidden = false
+		case 6:
+			storyTextView.text = story6
+			topButton.isHidden = true
+			bottomButton.isHidden = true
+			restartButton.isHidden = false
+		default:
+			print("Error")
+		}
+	}//updateUI()
+
+	func restartStory() {
+		
+		//set storyIndex to 1
+		storyIndex = 1
+		
+		//unhide option buttons
+		topButton.isHidden = false
+		bottomButton.isHidden = false
+		
+		//hide restart button
+		restartButton.isHidden = true
+		
+	}//restartStory()
+	
+}//ViewController: UIViewController
+
